a5694da70e8999509e672c25cfbae8df6e4f6429
Idan Gazit <idan@gazit.me>
2013-12-23 11:02:58 +0200

    Adding map

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 $(document).foundation();
+
+function initialize() {
+    var office = new google.maps.LatLng(32.1586959, 34.8066168);
+
+    var mapOptions = {
+        center: office,
+        zoom: 15
+    };
+
+    var map = new google.maps.Map(document.getElementById("contactmap"),
+        mapOptions);
+
+    var marker = new google.maps.Marker({
+        position: office,
+        map: map,
+        title:"Hahoshlim 6, Herzeliya Pituach"
+    });
+
+    var contentString = '<div class="mapinfo">HaHoshlim 6, Herzeliya Pituach</div>'
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+    infowindow.open(map,marker);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
